9c2b104c37797195ee383ce852a746527b17ee85
Sebastian Frysztak <sebfry@gmail.com>
2019-02-11 22:51:24 +0100

    MoviePreview: refine UI

--- src/components/MoviePreview.tsx
+++ src/components/MoviePreview.tsx
@@ -19,7 +19,7 @@ export const MoviePreview: React.FC<MoviePreviewProps> = props => {
   sections.push(`${props.movie.runtime} minut`);
 
   return (
-    <div>
+    <div className="movie-preview">
       <div className="w-100 d-none d-md-block movie-preview-overlay">
         <Image className="w-100" src={props.movie.hero_url_desktop} />
       </div>
@@ -30,11 +30,11 @@ export const MoviePreview: React.FC<MoviePreviewProps> = props => {
       <Carousel.Caption>
         <Container className="text-left">
           <Row>
-            <Col>
-              <h1>{props.movie.title_pl}</h1>
+            <Col className="movie-preview-title">
+              {props.movie.title_pl}
             </Col>
           </Row>
-          <Row className="d-none d-md-flex">
+          <Row className="d-none d-md-flex movie-preview-genres">
             {sections.map((section, i) => (
               <Col md="auto" key={i}>
                 {section}
@@ -46,44 +46,3 @@ export const MoviePreview: React.FC<MoviePreviewProps> = props => {
     </div>
   );
 };
-
-const MoviesCarousel = () => {
-  return (
-    <Carousel indicators={false}>
-      <Carousel.Item>
-        <Image
-          className="w-100"
-          src="https://multikino.pl/-/media/images/home-page/slider-hero-component/greenbook_slider-d.jpg"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://multikino.pl/-/media/images/home-page/slider-hero-component/lego2-slider-d.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://multikino.pl/-/media/images/home-page/slider-hero-component/kogel-slider2-d.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
-};
-
-export default MoviesCarousel;
