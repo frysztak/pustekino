f525e95c276b77376c5e4b57ddda85068ab89dd0
Sebastian Frysztak <sebfry@gmail.com>
2019-02-12 14:40:44 +0100

    MoviePoster: make card clickable

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -10,7 +10,9 @@ export const MoviePoster: React.FC<MoviePosterProps> = ({ movie }) => (
   <Card className="text-center mt-3 bg-transparent" style={{ width: "15em" }}>
     <Card.Img variant="top" src={movie.poster_url} height="300rem" />
     <Card.Body>
-      <Card.Title>{movie.title_pl}</Card.Title>
+      <a href="#" className="card-title h5 stretched-link">
+        {movie.title_pl}
+      </a>
     </Card.Body>
   </Card>
 );
